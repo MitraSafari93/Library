dfe969ea14be0d7c453d71fddafcb04d07c6bd45
louis-e <44675238+louis-e@users.noreply.github.com>
2019-08-20 23:28:22 +0200

    Updatet Form1.Designer.cs
    Removed unnecessary/unused Form1_Load

--- LibraryProject/LibraryProject/LibraryProject/Form1.Designer.cs
+++ LibraryProject/LibraryProject/LibraryProject/Form1.Designer.cs
@@ -99,7 +99,6 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Library";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
